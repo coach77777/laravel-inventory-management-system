bab9a2623473e5da151e0ddb6e3e77fb3e7e48f4
coach77777 <108881946+coach77777@users.noreply.github.com>
2024-11-03 19:26:46 -0600

    Manage Suppliers

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Demo\DemoController;
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\Pos\SupplierController;
 
 
 Route::get('/', function () {
@@ -12,11 +13,11 @@ Route::get('/', function () {
 
 Route::controller(DemoController::class)->group(function () {
     Route::get('/about', 'Index')->name('about.page')->middleware('check');
-    Route::get('/contact', 'ContactMethod')->name('cotact.page');
+    Route::get('/contact', 'ContactMethod')->name('contact.page');
 });
 
 
- // Admin All Route 
+ // Admin All Route
 Route::controller(AdminController::class)->group(function () {
     Route::get('/admin/logout', 'destroy')->name('admin.logout');
     Route::get('/admin/profile', 'Profile')->name('admin.profile');
@@ -25,11 +26,23 @@ Route::controller(AdminController::class)->group(function () {
 
     Route::get('/change/password', 'ChangePassword')->name('change.password');
     Route::post('/update/password', 'UpdatePassword')->name('update.password');
-     
+
 });
 
 
- 
+ // Admin All Route
+Route::controller(SupplierController::class)->group(function () {
+    Route::get('/supplier/all', 'SupplierAll')->name('supplier.all');
+    Route::get('/supplier/add', 'SupplierAdd')->name('supplier.add');
+    Route::post('/supplier/store', 'SupplierStore')->name('supplier.store');
+
+    Route::get('/supplier/edit/{id}', 'SupplierEdit')->name('supplier.edit');
+    Route::post('/supplier/update', 'SupplierUpdate')->name('supplier.update');
+
+    Route::get('/supplier/delete{id}', 'SupplierDelete')->name('supplier.delete');
+
+});
+
 
 
 
