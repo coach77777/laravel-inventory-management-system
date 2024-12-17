0c1f9d89b87ea31fe384ba79d9b7ee651ed26112
coach77777 <108881946+coach77777@users.noreply.github.com>
2024-12-17 06:51:41 -0600

    Manage Supplier Wise Stock Report

--- routes/web.php
+++ routes/web.php
@@ -143,6 +143,10 @@ Route::controller(StockController::class)->group(function () {
     Route::get('/stock/report', 'StockReport')->name('stock.report');
     Route::get('/stock/report/pdf', 'StockReportPdf')->name('stock.report.pdf');
 
+    Route::get('/stock/supplier/wise', 'StockSupplierWise')->name('stock.supplier.wise');
+    Route::get('/supplier/wise/pdf', 'SupplierWisePdf')->name('supplier.wise.pdf');
+    
+
 });
 
 
