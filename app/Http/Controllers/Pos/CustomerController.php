5c11c119d2f1c8b594f14a75351317320affd91e
coach77777 <108881946+coach77777@users.noreply.github.com>
2024-12-24 07:35:24 -0600

    Manage Customer Edit Invoice

--- app/Http/Controllers/Pos/CustomerController.php
+++ app/Http/Controllers/Pos/CustomerController.php
@@ -9,6 +9,7 @@ use Auth;
 use Illuminate\Support\Carbon;
 use Image;
 use App\Models\Payment;
+use App\Models\PaymentDetail;
 
 
 
@@ -146,6 +147,55 @@ public function CreditCustomerPrintPdf(){
     return view('backend.pdf.customer_credit_pdf',compact('allData'));
 
 }// End Method
+
+public function CustomerEditInvoice($invoice_id){
+
+    $payment = Payment::where('invoice_id',$invoice_id)->first();
+    return view('backend.customer.edit_customer_invoice',compact('payment'));
+
+}// End Method
+
+public function CustomerUpdateInvoice(Request $request,$invoice_id){
+    if ($request->new_paid_amount < $request->paid_amount) {
+        $notification = array(
+        'message' => 'Sorry You Paid Maximum Value',
+        'alert-type' => 'error'
+    );
+    return redirect()->back()->with($notification);
+    } else{
+
+        $payment = Payment::where('invoice_id',$invoice_id)->first();
+        $payment_details = new PaymentDetail();
+        $payment->paid_status = $request->paid_status;
+
+        if ($request->paid_status == 'full_paid') {
+            $payment->paid_amount = Payment::where('invoice_id',$invoice_id)->first()['paid_amount'] + $request->new_paid_amount;
+            $payment->due_amount = '0';
+            $payment_details->current_paid_amount = $request->new_paid_amount;
+
+
+        } elseif($request->paid_status == 'partial_paid'){
+            $payment->paid_amount = Payment::where('invoice_id',$invoice_id)->first()['paid_amount'] + $request->new_paid_amount;
+            $payment->due_amount = Payment::where('invoice_id',$invoice_id)->first()['due_amount'] - $request->new_paid_amount;
+            $payment_details->current_paid_amount = $request->new_paid_amount;
+        }
+
+        $payment->save();
+            $payment_details->invoice_id = $invoice_id;
+            $payment_details->date = date('Y-m-d',strtotime($request->date));
+            $payment_details->updated_by = Auth::user()->id;
+            $payment_details->save();
+
+        $notification = array(
+            'message' => 'Invoice Payment Updated Successfully',
+            'alert-type' => 'success'
+        );
+
+        return redirect()->route('credit.customer')->with($notification);
+
+    }
+}// End Method
+
 
 
 } // End Class
